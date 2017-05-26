01217385915f3b795d326c18c7083d22236d97ed
Vi Dao <vi.dao@navihealth.us>
2017-05-26 11:29:07 -0400

    1st try

--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { MyFoodLibrary } from '../../../bakery_data/allrecipes';
+
+const allFoodRecipes = new MyFoodLibrary().allRecipes;
 
 @Component({
   selector: 'page-list',
@@ -7,24 +10,19 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class ListPage {
   selectedItem: any;
-  icons: string[];
   items: Array<{title: string, note: string, icon: string}>;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     // If we navigated to this page, we will have an item available as a nav param
     this.selectedItem = navParams.get('item');
 
-    // Let's populate this page with some filler content for funzies
-    this.icons = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
-    'american-football', 'boat', 'bluetooth', 'build'];
-
     this.items = [];
-    for (let i = 1; i < 11; i++) {
+    for (let i = 0; i < allFoodRecipes.length; i++) {
       this.items.push({
-        title: 'Item ' + i,
-        note: 'This is item #' + i,
-        icon: this.icons[Math.floor(Math.random() * this.icons.length)]
-      });
+        title: allFoodRecipes[i].title.toString(),
+        note: allFoodRecipes[i].note.toString(),
+        icon: allFoodRecipes[i].icon.toString()
+      })
     }
   }
 
