eb06dae14201dfa4efd6827964917399b0754afe
Vi Dao <vi.dao@navihealth.us>
2017-05-31 14:48:07 -0400

    Add List Dividers

--- src/pages/all-recipes-list/all-recipes-list.ts
+++ src/pages/all-recipes-list/all-recipes-list.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { LoadingController, NavController, NavParams } from 'ionic-angular';
 import { RecipesApi } from '../../shared/shared';
 import { RecipeDetailViewPage } from '../pages';
-
-// const allFoodRecipes = new MyFoodLibrary().allRecipes;
+import * as _ from 'lodash';
 
 @Component({
   selector: 'page-list',
@@ -12,10 +11,12 @@ import { RecipeDetailViewPage } from '../pages';
 export class AllRecipesList {
   selectedItem: any;
   items: Array<{title: string, note: string, icon: string, category: string, ingredients: Array<{}>, directions: Array<{}>}>;
+  recipesCategories: any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private recipesApi: RecipesApi) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public loadingController: LoadingController, private recipesApi: RecipesApi) {
     this.selectedItem = navParams.get('item');
     this.items = [];
+    this.recipesCategories = [];
   }
 
   itemTapped(event, item) {
@@ -23,18 +24,28 @@ export class AllRecipesList {
   }
 
   ionViewDidLoad() {
-  this.recipesApi.getAllRecipes().subscribe(data => {
-    data.body.forEach(element => { this.items.push({
-      title: element.title,
-      note: element.note,
-      icon: element.icon,
-      category: element.category,
-      ingredients: element.ingredients,
-      directions: element.directions
-    })
-  }); 
-});
+    let loader = this.loadingController.create({
+      content: "Baking..."
+    });
 
+    loader.present().then(() => {
+          this.recipesApi.getAllRecipes().subscribe(data => {
+          data.body.forEach(element => { this.items.push({
+            title: element.title,
+            note: element.note,
+            icon: element.icon,
+            category: element.category,
+            ingredients: element.ingredients,
+            directions: element.directions
+          })
+        });
+
+        this.recipesCategories = _.chain(this.items).groupBy('category').toPairs().map(item => _.zipObject(['categoryName', 'categoryRecipes'], item )).value();
+        console.log(this.recipesCategories);
+
+        loader.dismiss(); 
+      });
+    })
 }
 
 }
